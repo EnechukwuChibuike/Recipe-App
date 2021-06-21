65dcb9ed1cd8ae013fd5063ef7cf390d5ce20aa4
EnechukwuChibuike <enechukwuchibuike@gmail.com>
2021-06-21 09:54:17 +0100

    Updates

---
+++ precache-manifest.20381219ade8bc61b8b74f18ba0e4b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050b717da7410d885f34780bc295b839",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "fe76ffe0816e56bcbf67",
+    "url": "/Recipe-App/static/css/main.d3e259a6.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/Recipe-App/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "fe76ffe0816e56bcbf67",
+    "url": "/Recipe-App/static/js/main.5b5c8eaa.chunk.js"
+  },
+  {
+    "revision": "7c6d1fd2d3863083c7ca",
+    "url": "/Recipe-App/static/js/runtime~main.38a06a2c.js"
+  }
+]);
